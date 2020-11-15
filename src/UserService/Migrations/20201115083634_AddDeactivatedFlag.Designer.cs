e92a8e8d8ad157b6a3a517d423cdb3f8ef3879a9
RyannnnnnR <ryan.reichenberg@gmail.com>
2020-11-15 19:41:07 +1100

    Update migrations to include deactivated flag

--- src/UserService/Migrations/20201115083634_AddDeactivatedFlag.Designer.cs
+++ src/UserService/Migrations/20201115083634_AddDeactivatedFlag.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserService.Repositories;
 
 namespace UserService.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201115083634_AddDeactivatedFlag")]
+    partial class AddDeactivatedFlag
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
