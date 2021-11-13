8dd3b5d723d22ac55c41ca5dcac7552c5f85dd06
Ryan Reichenberg <ryanreichenberg@ryans-mbp.lan>
2021-11-13 13:12:05 +1100

    Build out Payment service and invoicing service

--- src/PaymentsService/src/Startup.cs
+++ src/PaymentsService/src/Startup.cs
@@ -1,21 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PaymentsService.Services;
 
-namespace PaymentService
+namespace PaymentsService
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IStripeService, StripeService>();
+            services.AddHangfire(x => x.UseSqlServerStorage(_configuration.GetConnectionString("HangfireConnection")));
+            services.AddHangfireServer();
+            services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,8 +36,13 @@ namespace PaymentService
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHangfireDashboard();
+            RecurringJob.AddOrUpdate<IStripeService>(x => 
+                x.SendPayoutToAccount(), Cron.Daily, TimeZoneInfo.FindSystemTimeZoneById("Australia/Melbourne"));
+            
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
@@ -34,6 +50,8 @@ namespace PaymentService
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapControllers();
+                endpoints.MapHangfireDashboard();
             });
         }
     }
